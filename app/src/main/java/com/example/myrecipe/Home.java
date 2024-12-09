e56503b535513d0a4d72d66bab00b06f80b31411
Teacher <omer@benoualid.org>
2024-12-09 09:52:16 +0200

    תיקון באגים ומסך חיפוש

--- app/src/main/java/com/example/myrecipe/Home.java
+++ app/src/main/java/com/example/myrecipe/Home.java
@@ -9,19 +9,34 @@ import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
 
+    Button Search;
+    Button Log_out;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-       // Button buttonGoToSecond = findViewById(R.id.buttonGoToSecond);
-//        buttonGoToSecond.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(Home.this, SignIn.class);
-//                startActivity(intent);
-//            }
-//        });
+        Log_out = findViewById(R.id.Log_out);
+        Log_out.setSoundEffectsEnabled(false);
+
+        Log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, SignIn.class);
+                startActivity(intent);
+            }
+
+            Search = findViewById(R.id.Log_out);
+            Search.setSoundEffectsEnabled(false);
+
+             Search.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Home.this, Search1.class);
+                    startActivity(intent);
+
+        });
 
 
     }
