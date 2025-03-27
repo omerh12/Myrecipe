1e285fb62a289dde276594c01125e6d688ca3324
Naama <naamagr13@gmail.com>
2025-03-27 11:03:14 +0200

    adapter

--- app/src/main/java/com/example/myrecipe/adapter.java
+++ app/src/main/java/com/example/myrecipe/adapter.java
@@ -1,7 +1,14 @@
 package com.example.myrecipe;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -15,11 +22,21 @@ public class adapter extends ArrayAdapter<Recipe> {
         this.RecipeList = recipeList;
     }
 
+    public View getview(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = ((Activity) Context).getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.activity_matkon1, parent, false);
+        TextView recipe_name= (TextView) view.findViewById(R.id.recipe_name);
+        TextView recipe_ingredients= (TextView) view.findViewById(R.id.recipe_ingredients);
+        TextView recipe_instruction=(TextView) view.findViewById(R.id.recipe_instructions);
+
+        Recipe temp= RecipeList.get(position);
+        recipe_name.setText(temp.getName());
+        recipe_ingredients.setText(temp.getIngredients());
+        recipe_instruction.setText(temp.getInstructions());
+        return view;
 
 
+    }
+        }
 
 
-
-
-
-}
