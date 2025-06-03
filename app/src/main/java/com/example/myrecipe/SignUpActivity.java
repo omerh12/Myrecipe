d332a90f70e47b1045211bc9e6bded9191e7b0ac
David Benoualid <david.benoualid@hp.com>
2025-06-03 13:40:31 +0300

    last finishes!!

--- app/src/main/java/com/example/myrecipe/SignUpActivity.java
+++ app/src/main/java/com/example/myrecipe/SignUpActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
