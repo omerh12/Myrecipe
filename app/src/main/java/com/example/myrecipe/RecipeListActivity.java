a785eed3a5b9451d5c041e1f2fe7fa0f4bc50213
David Benoualid <david.benoualid@hp.com>
2025-06-06 10:48:36 +0300

    last finishes!!

--- app/src/main/java/com/example/myrecipe/RecipeListActivity.java
+++ app/src/main/java/com/example/myrecipe/RecipeListActivity.java
@@ -7,19 +7,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,11 +37,7 @@ public class RecipeListActivity extends AppCompatActivity {
         adapter = new RecipesAdapter(this, recipesList);
         recyclerView.setAdapter(adapter);
 
-
-
         database = FirebaseDatabase.getInstance().getReference("recipes");
-
-
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
